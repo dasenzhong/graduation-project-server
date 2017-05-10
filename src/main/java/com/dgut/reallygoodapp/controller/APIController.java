b40e2d1b57a1a021abf5498b13581810345630b9
dasenzhong <727745374@qq.com>
2017-05-11 01:20:13 +0800

    添加实体接口

--- src/main/java/com/dgut/reallygoodapp/controller/APIController.java
+++ src/main/java/com/dgut/reallygoodapp/controller/APIController.java
@@ -4,16 +4,32 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dgut.reallygoodapp.entity.Article;
 import com.dgut.reallygoodapp.entity.CompanyUser;
+import com.dgut.reallygoodapp.entity.Experience;
+import com.dgut.reallygoodapp.entity.Honor;
+import com.dgut.reallygoodapp.entity.Job;
+import com.dgut.reallygoodapp.entity.Post;
+import com.dgut.reallygoodapp.entity.Resume;
 import com.dgut.reallygoodapp.entity.StudentUser;
+import com.dgut.reallygoodapp.entity.Talk;
+import com.dgut.reallygoodapp.service.IArticleService;
 import com.dgut.reallygoodapp.service.ICompanyUserService;
+import com.dgut.reallygoodapp.service.IExperienceService;
+import com.dgut.reallygoodapp.service.IHonorService;
+import com.dgut.reallygoodapp.service.IJobService;
+import com.dgut.reallygoodapp.service.IPostService;
+import com.dgut.reallygoodapp.service.IResumeService;
 import com.dgut.reallygoodapp.service.IStudentUserService;
+import com.dgut.reallygoodapp.service.ITalkService;
 
 @RestController
 @RequestMapping("/api")
@@ -24,6 +40,27 @@ public class APIController {
 
 	@Autowired
 	ICompanyUserService CompanyUserService;
+	
+	@Autowired
+	IJobService JobService;
+	
+	@Autowired
+	IExperienceService ExperienceService;
+	
+	@Autowired
+	IHonorService HonorService;
+	
+	@Autowired
+	IPostService PostService;
+	
+	@Autowired
+	ITalkService TalkService;
+	
+	@Autowired
+	IArticleService ArticleService;
+	
+	@Autowired
+	IResumeService ResumeService;
 
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public @ResponseBody String hello() {
@@ -41,6 +78,11 @@ public class APIController {
 		studentUser.setAccount(account);
 		studentUser.setPasswordHash(password);
 		studentUser.setSex(sex);
+		
+		Resume resume = new Resume();
+		resume.setStudentUser(studentUser);
+		
+		studentUser.setResume(ResumeService.save(resume));
 
 		return StudentUserService.save(studentUser);
 
@@ -132,4 +174,182 @@ public class APIController {
 			request.getSession(true).removeAttribute("uid");
 		}
 	}
+	
+	///发布兼职
+	@RequestMapping(value="/addjob",method=RequestMethod.POST)
+	@ResponseBody
+	public Job addJob(
+			@RequestParam String jobName,
+			@RequestParam String jobType,
+			@RequestParam String number,
+			@RequestParam String education,
+			@RequestParam String money,
+			@RequestParam String describe,
+			@RequestParam String employProvince,
+			@RequestParam String employCity,
+			@RequestParam String employTown,
+			@RequestParam String workProvince,
+			@RequestParam String workCity,
+			@RequestParam String workTown,
+			@RequestParam String workAddress,
+			HttpServletRequest request){
+		
+		CompanyUser companyUser = checkLoginCompany(request);
+		
+		Job job = new Job();
+		
+		job.setJobName(jobName);
+		job.setJobType(jobType);
+		job.setNumber(number);
+		job.setEducation(education);
+		job.setMoney(money);
+		job.setDecribe(describe);
+		job.setEmployProvince(employProvince);
+		job.setEmployCity(employCity);
+		job.setEmployTown(employTown);
+		job.setWorkProvince(workProvince);
+		job.setWorkCity(workCity);
+		job.setWorkTown(workTown);
+		job.setWorkAddress(workAddress);
+		
+		job.setCompanyUser(companyUser);
+		
+		return JobService.save(job);
+		
+	}
+	
+	@RequestMapping(value="/addexperience",method=RequestMethod.POST)
+	@ResponseBody
+	public Experience addExperience(
+			@RequestParam String startTime,
+			@RequestParam String endTime,
+			@RequestParam String companyName,
+			@RequestParam String companyPost,
+			@RequestParam String describe,
+			@RequestParam String type,
+			HttpServletRequest request){
+		
+		Resume resume = ResumeService.findByStudentUser(checkLoginStudent(request));
+		
+		Experience experience = new Experience();
+		
+		experience.setStartTime(startTime);
+		experience.setEndTime(endTime);
+		experience.setCompanyName(companyName);
+		experience.setCompanyPost(companyPost);
+		experience.setDescribe(describe);
+		experience.setType(type);
+		
+		experience.setResume(resume);
+		
+		return ExperienceService.save(experience);
+		
+	}
+	
+	//添加荣誉
+	@RequestMapping(value="/addhonor",method=RequestMethod.POST)
+	@ResponseBody
+	public Honor addHonor(
+			@RequestParam String time,
+			@RequestParam String honorName,
+			@RequestParam String level,
+			HttpServletRequest request){
+		
+		Resume resume = ResumeService.findByStudentUser(checkLoginStudent(request));
+		
+		Honor honor = new Honor();
+
+		honor.setTime(time);
+		honor.setHonorName(honorName);
+		honor.setLevel(level);
+		
+		honor.setResume(resume);
+		
+		return HonorService.save(honor);
+		
+	}
+	
+	//添加职务
+	@RequestMapping(value="/addpost",method=RequestMethod.POST)
+	@ResponseBody
+	public Post addPost(
+			@RequestParam String startTime,
+			@RequestParam String endTime,
+			@RequestParam String postName,
+			@RequestParam String describe,
+			HttpServletRequest request){
+		
+		Resume resume = ResumeService.findByStudentUser(checkLoginStudent(request));
+		
+		Post post = new Post();
+		
+		post.setStartTime(startTime);
+		post.setEndTime(endTime);
+		post.setPostName(postName);
+		post.setDescribe(describe);
+		
+		post.setResume(resume);
+		
+		return PostService.save(post);
+		
+	}
+	
+	//添加文章
+	@RequestMapping(value = "/addarticle",method=RequestMethod.POST)
+	@ResponseBody
+	public Article addArticle(
+			@RequestParam String articleString,
+			HttpServletRequest request){
+		
+		CompanyUser companyUser = checkLoginCompany(request);
+		StudentUser studentUser = checkLoginStudent(request);
+		
+		Article article = new Article();
+		article.setArticle(articleString);
+		
+		if (companyUser!=null) {
+			article.setUserId(companyUser.getId());
+			article.setUserAccount(companyUser.getAccount());
+		}
+		if (studentUser != null) {
+			article.setUserId(studentUser.getId());
+			article.setUserAccount(studentUser.getAccount());
+		}
+		
+		return ArticleService.save(article);
+	}
+	
+	//添加随想
+	@RequestMapping(value = "/addtalk",method=RequestMethod.POST)
+	@ResponseBody
+	public Talk addTalk(
+			@RequestParam String talkString,
+			HttpServletRequest request){
+		
+		CompanyUser companyUser = checkLoginCompany(request);
+		StudentUser studentUser = checkLoginStudent(request);
+		
+		Talk talk = new Talk();
+		talk.setTalk(talkString);
+		
+		if (companyUser!=null) {
+			talk.setUserId(companyUser.getId());
+			talk.setUserAccount(companyUser.getAccount());
+		}
+		if (studentUser != null) {
+			talk.setUserId(studentUser.getId());
+			talk.setUserAccount(studentUser.getAccount());
+		}
+		
+		return TalkService.save(talk);
+	}
+	
+	//获取最新的兼职信息
+	@RequestMapping(value="/getnewjob/{page}",method=RequestMethod.GET)
+	@ResponseBody
+	public Page<Job> getNewJob(@PathVariable int page){
+		return JobService.getByCreateDatePage(page);
+	}
+	
+	
 }
