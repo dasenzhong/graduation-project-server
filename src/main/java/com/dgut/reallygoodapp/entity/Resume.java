bbe12055e20d6e687bfd7f0c98a4b64ee2607d08
dasenzhong <727745374@qq.com>
2017-05-13 18:53:27 +0800

    修改了简历实体

--- src/main/java/com/dgut/reallygoodapp/entity/Resume.java
+++ src/main/java/com/dgut/reallygoodapp/entity/Resume.java
@@ -1,6 +1,7 @@
 package com.dgut.reallygoodapp.entity;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
@@ -10,7 +11,78 @@ import com.dgut.reallygoodapp.util.BaseEntity;
 @Entity
 public class Resume extends BaseEntity {
 
+	String name;				//简历名字
+	String birthday;			//出生年月
+	String tlelephone;			//电话
+	String liveProvince;		//省份
+	String liveCity;			//城市
+	String liveTown;			//城镇
+	String school;				//学校
+	
 	StudentUser studentUser;
+
+	@Column(nullable=true)
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Column(nullable=true)
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+	@Column(nullable=true)
+	public String getTlelephone() {
+		return tlelephone;
+	}
+
+	public void setTlelephone(String tlelephone) {
+		this.tlelephone = tlelephone;
+	}
+
+	@Column(nullable=true)
+	public String getLiveProvince() {
+		return liveProvince;
+	}
+
+	public void setLiveProvince(String liveProvince) {
+		this.liveProvince = liveProvince;
+	}
+
+	@Column(nullable=true)
+	public String getLiveCity() {
+		return liveCity;
+	}
+
+	public void setLiveCity(String liveCity) {
+		this.liveCity = liveCity;
+	}
+
+	@Column(nullable=true)
+	public String getLiveTown() {
+		return liveTown;
+	}
+
+	public void setLiveTown(String liveTown) {
+		this.liveTown = liveTown;
+	}
+
+	@Column(nullable=true)
+	public String getSchool() {
+		return school;
+	}
+
+	public void setSchool(String school) {
+		this.school = school;
+	}
 
 	@OneToOne(optional=false,cascade=CascadeType.REFRESH)
 	@JoinColumn(name="student_id",referencedColumnName="id",unique=true)
